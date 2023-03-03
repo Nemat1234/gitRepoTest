fc6119691fe2fb817b0a3d85bccef6105147fec7
Nemat1234 <nematullah.yaseer47@gmail.com>
2023-03-02 22:20:36 -0800

    new practice

--- src/test/git/GitTest.java
+++ src/test/git/GitTest.java
@@ -7,5 +7,8 @@ public class GitTest {
 	public void test43() {
 		System.out.println("thi is class practice");
 	}
+	public void test45() {
+		System.out.println("this is only for practice");
+	}
 
 }
