fd3115b1fa301c45b245cb9826bef5e82fd664c9
Nemat1234 <nematullah.yaseer47@gmail.com>
2023-03-02 22:01:28 -0800

    first practice

--- src/test/git/GitTest.java
+++ src/test/git/GitTest.java
@@ -1,5 +1,11 @@
 package test.git;
 
 public class GitTest {
+	public void test (int a , int b) {
+		System.out.println("thi is just to test");
+	}
+	public void test1() {
+		System.out.println("thi is class practice");
+	}
 
 }
